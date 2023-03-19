d57b707d364629ba6561c97e9d377300fa60b321
Char2s <Char2s@outlook.com>
2023-03-19 03:21:33 -0400

    chore(angular): prepare deps for the dev app

--- packages/angular-app/src/app/app.module.ts
+++ packages/angular-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutProjectionModule } from '@layout-projection/angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LayoutProjectionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
