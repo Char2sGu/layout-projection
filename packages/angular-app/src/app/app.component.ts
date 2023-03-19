53d7cf430a26a10f0aa034799e2377c5b13eda81
Char2s <Char2s@outlook.com>
2023-03-19 04:13:08 -0400

    chore(angular): add simple example in the dev app

--- packages/angular-app/src/app/app.component.ts
+++ packages/angular-app/src/app/app.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``,
-  styles: [``],
+  template: `
+    <div class="container" lpjNode [animateOn]="flag">
+      <div class="content" [class.flag]="flag" lpjNode></div>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        position: relative;
+        width: 500px;
+        height: 500px;
+        background-color: purple;
+      }
+
+      .content {
+        position: absolute;
+        background-color: white;
+      }
+      .content:not(.flag) {
+        top: 100px;
+        left: 100px;
+        width: 100px;
+        height: 100px;
+      }
+      .content.flag {
+        bottom: 100px;
+        right: 100px;
+        width: 200px;
+        height: 150px;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  flag = false;
+
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.flag = !this.flag;
+      this.changeDetector.markForCheck();
+    }, 1000);
+  }
+}
