e1c5dc802eabaa20abeeee48f390875f6f69b404
Char2s <Char2s@outlook.com>
2023-03-23 23:09:27 -0400

    feat(angular): avoid saving node snapshots outside of the scope

--- packages/angular/src/layout-animation-scope.directive.ts
+++ packages/angular/src/layout-animation-scope.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
-import { Node, NodeSnapshotMap } from '@layout-projection/core';
+import { Directive, inject } from '@angular/core';
+import { Node, NodeSnapshot, NodeSnapshotMap } from '@layout-projection/core';
 
 import { LayoutAnimationEntryDirective } from './layout-animation-entry.directive';
 
-export class LayoutAnimationScopeNodeRegistry extends Set<Node> {}
+export class LayoutAnimationScopeNodeRegistry extends Set<Node> {
+  private idSet = new Set<string>();
+  override add(value: Node): this {
+    this.idSet.add(value.id);
+    return super.add(value);
+  }
+  override delete(value: Node): boolean {
+    this.idSet.delete(value.id);
+    return super.delete(value);
+  }
+  override clear(): void {
+    this.idSet.clear();
+    super.clear();
+  }
+  hasId(id: string): boolean {
+    return this.idSet.has(id);
+  }
+}
+
 export class LayoutAnimationScopeEntryRegistry extends Set<LayoutAnimationEntryDirective> {}
+
+export class LayoutAnimationScopedNodeSnapshotMap extends NodeSnapshotMap {
+  constructor(private nodeRegistry: LayoutAnimationScopeNodeRegistry) {
+    super();
+  }
+  override set(key: string, value: NodeSnapshot): this {
+    if (!this.nodeRegistry.hasId(key)) return this;
+    return super.set(key, value);
+  }
+}
 
 @Directive({
   selector: '[lpjAnimationScope]',
@@ -19,7 +47,10 @@ export class LayoutAnimationScopeEntryRegistry extends Set<LayoutAnimationEntryD
     },
     {
       provide: NodeSnapshotMap,
-      useValue: new NodeSnapshotMap(),
+      useFactory: () =>
+        new LayoutAnimationScopedNodeSnapshotMap(
+          inject(LayoutAnimationScopeNodeRegistry),
+        ),
     },
   ],
 })
