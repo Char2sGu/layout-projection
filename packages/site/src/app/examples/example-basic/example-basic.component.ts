27b52de9e3deac3c2c39ecac7a1c9304736a047d
Char2s <Char2s@outlook.com>
2023-04-15 05:01:48 -0400

    feat(site): implement basic example draft

--- packages/site/src/app/examples/example-basic/example-basic.component.ts
+++ packages/site/src/app/examples/example-basic/example-basic.component.ts
@@ -1,11 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  Type,
+} from '@angular/core';
+import { TuiDocExample } from '@taiga-ui/addon-doc';
 
 @Component({
   selector: 'lpj-example-basic',
   templateUrl: './example-basic.component.html',
   styleUrls: ['./example-basic.component.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExampleBasicComponent {
+export class ExampleBasicComponent implements OnInit {
+  example?: TuiDocExample;
+  component?: Type<unknown>;
 
+  constructor(
+    private httpClient: HttpClient,
+    private changeDetector: ChangeDetectorRef,
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const code = await this.httpClient
+      .get(
+        'assets/examples/angular/basic/basic.example.ts', //
+        { responseType: 'text' },
+      )
+      .toPromise();
+    this.example = { ['TypeScript']: code };
+    this.component = await import(
+      '@layout-projection/angular-examples/basic'
+    ).then((m) => m.entry);
+    this.changeDetector.markForCheck();
+  }
 }
--- packages/site/src/app/examples/examples.module.ts
+++ packages/site/src/app/examples/examples.module.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TuiAddonDocModule } from '@taiga-ui/addon-doc';
 
+import { ExampleBasicComponent } from './example-basic/example-basic.component';
 import { ExampleTabsComponent } from './example-tabs/example-tabs.component';
 import { ExamplesComponent } from './examples.component';
 import { ExamplesRoutingModule } from './examples-routing.module';
-import { ExampleBasicComponent } from './example-basic/example-basic.component';
 
 @NgModule({
-  declarations: [ExamplesComponent, ExampleTabsComponent, ExampleBasicComponent],
-  imports: [CommonModule, ExamplesRoutingModule, TuiAddonDocModule],
+  declarations: [
+    ExamplesComponent,
+    ExampleTabsComponent,
+    ExampleBasicComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ExamplesRoutingModule,
+    TuiAddonDocModule,
+  ],
 })
 export class ExamplesModule {}
