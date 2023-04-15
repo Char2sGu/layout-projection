b94eaa9188224f700844f10d95eaf4a1072f418a
Char2s <Char2s@outlook.com>
2023-04-15 06:18:25 -0400

    feat(angular-examples): separate example into multiple files

--- packages/angular-examples/src/basic/basic.example.ts
+++ packages/angular-examples/src/basic/basic.example.ts
@@ -6,59 +6,8 @@ import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'ex-basic',
   standalone: true,
-  template: `
-    <ng-container *lpjAnimationScope>
-      <div
-        class="container"
-        [class.flag]="flag$ | async"
-        (click)="flag$.next(!flag$.value)"
-      >
-        <div class="box" lpjNode lpjAnimation [lpjAnimationTrigger]="flag$">
-          <div class="circle" lpjNode></div>
-        </div>
-      </div>
-    </ng-container>
-  `,
-  styles: [
-    `
-      :host {
-        display: block;
-        height: 100%;
-        background-color: purple;
-      }
-
-      .container {
-        height: 100%;
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-      }
-      .container.flag {
-        justify-content: flex-end;
-      }
-
-      .box {
-        display: flex;
-        width: 50%;
-        height: 100%;
-        justify-content: center;
-        align-items: center;
-        background-color: black;
-      }
-      .flag .box {
-        width: 30%;
-        height: 50%;
-        border-radius: 30%;
-      }
-
-      .circle {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        background-color: white;
-      }
-    `,
-  ],
+  templateUrl: './basic.example.html',
+  styleUrls: ['./basic.example.less'],
   imports: [CommonModule, LayoutProjectionModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
