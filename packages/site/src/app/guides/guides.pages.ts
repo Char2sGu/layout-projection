8dcc50f4d2a4877939b6fed6d411c2cd76aded32
Char2s <Char2s@outlook.com>
2023-04-22 11:14:01 -0400

    feat(site): reorganize pages

--- packages/site/src/app/guides/guides.pages.ts
+++ packages/site/src/app/guides/guides.pages.ts
@@ -2,22 +2,22 @@ import { TuiDocPage } from '@taiga-ui/addon-doc';
 
 export const GUIDES_PAGES: TuiDocPage[] = [
   {
-    section: 'Core',
+    section: 'Getting Started',
     title: 'Layout Projection',
     route: 'guides/core/layout-projection',
   },
   {
-    section: 'Core',
+    section: 'Getting Started',
+    title: 'Standalone Usage',
+    route: 'guides/core/standalone-usage',
+  },
+  {
+    section: 'Developer Guides',
     title: 'Layout Animation',
     route: 'guides/core/layout-animation',
   },
   {
-    section: 'Core',
-    title: 'Dependency Management',
-    route: 'guides/core/dependency-management',
-  },
-  {
-    section: 'Core',
+    section: 'Developer Guides',
     title: 'Snapshots',
     route: 'guides/core/snapshots',
   },
