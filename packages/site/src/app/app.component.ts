af9592cfb8ec85e645cbe4b12203a8d6eded2dd9
Char2s <Char2s@outlook.com>
2023-05-09 07:21:05 -0400

    feat(site): make header collapsable

--- packages/site/src/app/app.component.ts
+++ packages/site/src/app/app.component.ts
@@ -9,7 +9,14 @@ import { DOCUMENT } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { WINDOW } from '@ng-web-apis/common';
-import { BehaviorSubject, distinctUntilChanged, fromEvent, map } from 'rxjs';
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  fromEvent,
+  map,
+  Observable,
+  pairwise,
+} from 'rxjs';
 
 @Component({
   selector: 'lpj-root',
@@ -18,17 +25,24 @@ import { BehaviorSubject, distinctUntilChanged, fromEvent, map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger('header', [
-      state('detached', style({ boxShadow: 'var(--lpj-elevation)' })),
       state('regular', style({})),
+      state('detached', style({ boxShadow: 'var(--lpj-elevation)' })),
+      state('collapsed', style({ transform: 'translateY(-100%)' })),
       transition('regular <=> detached', [animate(`200ms linear`)]),
+      transition('collapsed <=> detached', [animate(`200ms ease-in-out`)]),
     ]),
   ],
 })
 export class AppComponent {
   documentScrollTop$ = new BehaviorSubject(0);
 
-  headerDetached$ = this.documentScrollTop$.pipe(
-    map((scrollTop) => scrollTop > 10),
+  headerState$: Observable<string> = this.documentScrollTop$.pipe(
+    pairwise(),
+    map(([prev, curr]) => {
+      if (curr > prev) return 'collapsed';
+      if (curr > 10) return 'detached';
+      return 'regular';
+    }),
     distinctUntilChanged(),
   );
 
