1fca8932bab0a89c34ce634fec4202e2b9179299
Char2s <char2s@outlook.com>
2024-10-26 14:20:03 -0400

    fix(core): export the border radius component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './animation.js';
 export * from './animation-core.js';
 export * from './animation-engines.js';
+export * from './components/border-radius.js';
 export * from './projection.js';
 export * from './projection-core.js';
 export * from './snapshot.js';
