4da864c8e115fadd08193c1f44ec3001386aaf59
Char2s <char2s@outlook.com>
2024-11-16 18:00:56 -0500

    feat(core): make coordinate equatable

--- packages/core/src/coordinate.ts
+++ packages/core/src/coordinate.ts
@@ -1,6 +1,11 @@
+import { Equatable } from './equatable.js';
+
 /**
  * A coordinate in 2D space.
  */
-export class Coordinate {
+export class Coordinate implements Equatable {
   constructor(readonly x: number, readonly y: number) {}
+  equals(other: this): boolean {
+    return this.x === other.x && this.y === other.y;
+  }
 }
