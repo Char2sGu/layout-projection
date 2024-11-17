0671fa25d22395e8da48f6800a4cb45ff5e6f667
Char2s <char2s@outlook.com>
2024-11-16 21:07:00 -0500

    feat(angular): add `provideDebuggingMetadataManager`

--- packages/angular/src/debugging.ts
+++ packages/angular/src/debugging.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { inject, Provider } from '@angular/core';
 import {
+  MetadataManager,
   ProjectionTreeAnimationConfig,
   ProjectionTreeAnimator,
 } from '@layout-projection/animation';
@@ -16,10 +17,31 @@ export function provideDebuggingProjectionTreeAnimator(): Provider[] {
       provide: ProjectionTreeAnimator,
       useFactory: (
         base = inject(ProjectionTreeAnimator, { skipSelf: true }),
-      ) => ({
+      ): ProjectionTreeAnimator => ({
         animate(config: ProjectionTreeAnimationConfig) {
-          console.debug(config);
+          console.debug('animate:', config);
           return base.animate(config);
+        },
+      }),
+    },
+  ];
+}
+
+export function provideDebuggingMetadataManager(): Provider[] {
+  return [
+    {
+      provide: MetadataManager,
+      useFactory: (
+        base = inject(MetadataManager, { skipSelf: true }),
+      ): MetadataManager => ({
+        define(target, token, value) {
+          console.debug('metadata:define', { target, token, value });
+          base.define(target, token, value);
+        },
+        resolve(target, token) {
+          const result = base.resolve(target, token);
+          console.debug('metadata:resolve', { target, token, result });
+          return result;
         },
       }),
     },
